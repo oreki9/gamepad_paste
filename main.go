7a54c6c0620955552ddb4df2c2f63a74463d8aa5
oreki9 <44063419+oreki9@users.noreply.github.com>
2025-06-27 22:32:42 +0700

    add hide output

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	// "fmt"
 	"os/exec"
 	"log"
+	"io"
 )
 
 // KeyHandler is the signature we’ll use for one-shot callbacks.
@@ -169,10 +170,14 @@ func main() {
 					inputText += " "
 				case 5:
 					cmd := exec.Command("bash", "-c", `echo "`+inputText+`" | xclip -selection clipboard`)
+					cmd.Stdout = io.Discard
+    				cmd.Stderr = io.Discard
 					if err := cmd.Run(); err != nil {
 						log.Fatalf("Failed to run command: %v", err)
 					}
 					cmd2 := exec.Command("bash", "-c", `nohup ./command.sh >/dev/null 2>&1`)
+					cmd2.Stdout = io.Discard
+    				cmd2.Stderr = io.Discard
 					if err2 := cmd2.Run(); err2 != nil {
 						log.Fatalf("Failed to run command: %v", err2)
 					}
