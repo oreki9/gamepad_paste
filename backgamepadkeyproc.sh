f8a951673a2c7eb3334c4670a31e2cd5e640ad66
oreki9 <44063419+oreki9@users.noreply.github.com>
2025-06-28 11:56:11 +0700

    add check evtest

--- backgamepadkeyproc.sh
+++ backgamepadkeyproc.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 gamepadWindow="main"
-sudo evtest /dev/input/event6 | grep --line-buffered 'code 315.*value 1' | while read line; do
-    echo "KEY 315 PRESSED!"
-    matches=$(ps aux | grep -i "$gamepadWindow" | grep -v grep)
-    checkprocess=$(echo "$matches" | wc -l)
-    if [ "$checkprocess" -eq 1 ]; then #process not found
-        kdocker "$gamepadWindow"
-    fi
-    # put your command here, e.g.:
-    # notify-send "Key 315 pressed"
-done
+if [ -e /dev/input/event6 ]; then
+    sudo evtest /dev/input/event6 | grep --line-buffered 'code 315.*value 1' | while read line; do
+        echo "KEY 315 PRESSED!"
+        matches=$(ps aux | grep -i "$gamepadWindow" | grep -v grep)
+        checkprocess=$(echo "$matches" | wc -l)
+        if [ "$checkprocess" -eq 1 ]; then #process not found
+            kdocker "$gamepadWindow"
+        fi
+        # put your command here, e.g.:
+        # notify-send "Key 315 pressed"
+    done
+fi
