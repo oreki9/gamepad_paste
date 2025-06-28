440830c0a5d27ebb9eff92cfb3f2f9deb0c767f2
oreki9 <44063419+oreki9@users.noreply.github.com>
2025-06-28 16:36:57 +0700

    fix something

--- backgamepadkeyproc.sh
+++ backgamepadkeyproc.sh
@@ -2,10 +2,9 @@
 gamepadWindow="gamepadkey"
 mainproc=$(ps aux | grep -i backgamepadkeyproc.sh | grep -v grep)
 checkmainprco=$(echo "$mainproc" | wc -l)
-echo "$checkmainprco"
 if [ "$checkmainprco" -eq 2 ]; then
     if [ -e /dev/input/event6 ]; then
-        sudo evtest /dev/input/event6 | grep --line-buffered 'code 315.*value 1' | while read line; do
+        evtest /dev/input/event6 | grep --line-buffered 'code 315.*value 1' | while read line; do
             echo "KEY 315 PRESSED!"
             matches=$(ps aux | grep -i "$gamepadWindow" | grep -v grep)
             checkprocess=$(echo "$matches" | wc -l)
@@ -19,7 +18,6 @@ if [ "$checkmainprco" -eq 2 ]; then
         echo "device is not found"
     fi
 else
-    echo "$checkmainprco" -gt 2
     if [ "$checkmainprco" -gt 2 ]; then
         themostold=$(echo "$mainproc" | awk 'NR > 1 { if ($10 > max) { max = $10; line = $0 } } END { print line }' | awk '{print $2}')
         countoldproc=$(echo "$themostold" | wc -l)
