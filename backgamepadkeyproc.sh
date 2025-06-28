2c5f3bf5334e5a6c308076384522a6e36ea4fedd
oreki9 <44063419+oreki9@users.noreply.github.com>
2025-06-28 15:19:11 +0700

    run command

--- backgamepadkeyproc.sh
+++ backgamepadkeyproc.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 gamepadWindow="gamepadkey"
-if [ -e /dev/input/event6 ]; then
-    sudo evtest /dev/input/event6 | grep --line-buffered 'code 315.*value 1' | while read line; do
-        echo "KEY 315 PRESSED!"
-        matches=$(ps aux | grep -i "$gamepadWindow" | grep -v grep)
-        checkprocess=$(echo "$matches" | wc -l)
-        if [ "$checkprocess" -eq 1 ]; then #process not found
-            kdocker "$gamepadWindow"
-        fi
-        # put your command here, e.g.:
-        # notify-send "Key 315 pressed"
-    done
+mainproc=$(ps aux | grep -i backgamepadkeyproc.sh | grep -v grep)
+checkmainprco=$(echo "$mainproc" | wc -l)
+if [ "$checkmainprco" -gt 2 ]; then
+    if [ -e /dev/input/event6 ]; then
+        sudo evtest /dev/input/event6 | grep --line-buffered 'code 315.*value 1' | while read line; do
+            echo "KEY 315 PRESSED!"
+            matches=$(ps aux | grep -i "$gamepadWindow" | grep -v grep)
+            checkprocess=$(echo "$matches" | wc -l)
+            if [ "$checkprocess" -eq 1 ]; then #process not found
+                kdocker "$gamepadWindow"
+            fi
+            # put your command here, e.g.:
+            # notify-send "Key 315 pressed"
+        done
+    else
+        echo "device is not found"
+    fi
 else
-    echo "device is not found"
+    themostold=$(echo "$mainproc" | awk 'NR > 1 { if ($10 > max) { max = $10; line = $0 } } END { print line }' | awk '{print $2}')
+    countoldproc=$(echo "$themostold" | wc -l)
+    if [ "$countoldproc" -eq 1 ]; then
+        kill "$themostold"
+    fi
 fi
---
+++ mainrun.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root (e.g., with sudo)"
+  exit 1
+else
+    if [ -e gamepadkey ]; then
+        sudo kdocker gamepadkey
+    else
+        go build -o gamepadkey
+        sudo kdocker gamepadkey
+    fi
+fi
