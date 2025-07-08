52a38f714193977b9f407d14eaacec508136e293
oreki <topikuputih@gmail.com>
2025-07-08 22:32:18 +0800

    add something

--- backgamepadkeyproc.sh
+++ backgamepadkeyproc.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 mainrun(){
     gamepadWindow="maingamepadkey"
-    mainproc=$(ps aux | grep -i backgamepadkeyproc.sh | grep -v grep)
+    mainproc=$(ps aux | grep -i "/bin/bash backgamepadkeyproc.sh" | grep -v grep)
     checkmainprco=$(echo "$mainproc" | wc -l)
-    if [ "$checkmainprco" -eq 2 ]; then
+    echo "$mainproc"
+    if [ "$checkmainprco" -eq 1 ]; then
         if [ -e /dev/input/event6 ]; then
             evtest /dev/input/event6 | grep --line-buffered 'code 315.*value 1' | while read line; do
                 echo "KEY 315 PRESSED!"
-                matches=$(ps aux | grep -i "$gamepadWindow" | grep -v grep)
+                matches=$(ps aux | grep -i "/bin/bash $gamepadWindow" | grep -v grep)
                 checkprocess=$(echo "$matches" | wc -l)
-                if [ "$checkprocess" -eq 1 ]; then #process not found
+                if [ "$checkprocess" -eq 0 ]; then #process not found
                     kdocker "./$gamepadWindow"
                     mainrun
                 fi
@@ -20,7 +21,8 @@ mainrun(){
             echo "device is not found"
         fi
     else
-        if [ "$checkmainprco" -gt 2 ]; then
+        echo "$checkmainprco"
+        if [ "$checkmainprco" -gt 1 ]; then
             themostold=$(echo "$mainproc" | awk 'NR > 1 { if ($10 > max) { max = $10; line = $0 } } END { print line }' | awk '{print $2}')
             countoldproc=$(echo "$themostold" | wc -l)
             if [ "$countoldproc" -eq 1 ]; then
@@ -30,3 +32,4 @@ mainrun(){
         fi
     fi
 }
+mainrun
